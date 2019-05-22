6ff9bd25c59e1b384a93ed7cd88931710b0c556b
Colin-Lacey <50182812+Colin-Lacey@users.noreply.github.com>
2019-05-22 10:18:38 -0600

    Add files via upload

---
+++ ScholarshipAwarder/src/Scholarship.java
@@ -0,0 +1,13 @@
+
+public class Scholarship {
+	private int minGPA;
+	private int faculty;
+	private int academicLevel;
+	
+	
+	public Scholarship(int minGPA, int faculty, int academicLevel) {
+		this.minGPA = minGPA;
+		this.faculty = faculty;
+		this.academicLevel = academicLevel;
+	}
+}
