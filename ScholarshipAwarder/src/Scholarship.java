0bde0ae4a81173fac3f6dc8ff5111d2c53eb6058
VinceDerayunan <Applepierocket@gmail.com>
2019-05-29 13:24:39 -0600

    <Changed int to string for faculty>

--- ScholarshipAwarder/src/Scholarship.java
+++ ScholarshipAwarder/src/Scholarship.java
@@ -1,17 +1,17 @@
 
 public class Scholarship {
 	private int minGPA;
-	private int faculty;
+	private String faculty;
 	private int academicLevel;
 	private int id;
-	
-	
-	public Scholarship(int minGPA, int faculty, int academicLevel) {
+
+
+	public Scholarship(int minGPA, String faculty, int academicLevel) {
 		this.minGPA = minGPA;
 		this.faculty = faculty;
 		this.academicLevel = academicLevel;
 	}
-	
+
 	public int getGPA() {
 		return this.minGPA;
 	}
